aeecfc7da964af7d3a41a9c5ff30e02b9bd457af
Conor Rafferty <conor@conor-VirtualBox>
2019-07-09 11:28:52 +0100

    new buttons

--- hello.c
+++ hello.c
@@ -1,6 +1,28 @@
-#include <stdio.h>
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+<style>
+    body {
+    background-color: lightblue;
+    }
 
-void main()
-{
-	printf("hello");
-}
+    h1 {
+    color: navy;
+    font-size: 35px;
+        text-align: center;
+    }
+    h2 {
+        color: mediumpurple;
+        font-size: 20px;
+        text-align: center;
+    }
+</style>
+    <meta charset="UTF-8">
+    <title>Sean Pettit The RA</title>
+</head>
+<body>
+<h1><big><strong>Sean Pettit</strong></big></h1> </p>
+<h2>Sean Pettit is an RA in CTYI (2019 Session 1)</h2> </p>
+<a href="Chants.html"> Sean Chants</a>
+<a href="Song.html"> Sean Song</a>
+</body>
